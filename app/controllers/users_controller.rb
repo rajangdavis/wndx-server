e47b2924561c2664c2d68632727b1aed22bcb3a3
rajangdavis <rajangdavis@gmail.com>
2014-12-16 03:47:04 -0800

    resetting users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,6 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      respond_to do |format|
-        format.json { render json: user, status: :created, location: user }
-      end
       session[:user_id] = user.id
       redirect_to notes_path
     else
