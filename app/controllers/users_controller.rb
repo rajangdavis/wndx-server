8dfca6fcd8d4f6fc9f053914a3d6f840f2d40403
rajangdavis <rajangdavis@gmail.com>
2014-12-16 03:38:32 -0800

    setting up users to be created via ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,9 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      respond_to do |format|
+        format.json { render json: user, status: :created, location: user }
+      end
       session[:user_id] = user.id
       redirect_to notes_path
     else
