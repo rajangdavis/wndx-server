26975b04abec3b5e0e6f12dd24686275573a63f6
rajangdavis <rajangdavis@gmail.com>
2014-12-17 09:30:41 -0800

    removed CORS restrictions in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   helper_method :current_user
 
-  before_filter :cors_preflight_check
+  before_filter :cors_preflight_check, :allow_iframe_requests
   after_filter :cors_set_access_control_headers
  
   def cors_set_access_control_headers
@@ -18,13 +18,18 @@ class ApplicationController < ActionController::Base
     if request.method == 'OPTIONS'
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Allow-Headers'] = 'ALLOWALL'
       headers['Access-Control-Max-Age'] = '1728000'
  
       render :text => '', :content_type => 'text/plain'
     end
   end
-  
+
+def allow_iframe_requests
+  response.headers.delete('X-Frame-Options')
+end
+
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id] 
   end
