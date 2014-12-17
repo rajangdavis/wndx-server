a44dc4465fda243f7e9d7730c9ccfffcfd8a42b0
rajangdavis <rajangdavis@gmail.com>
2014-12-17 09:44:20 -0800

    replaced my views with wndx stuff

--- public/wndx.js
+++ public/wndx.js
@@ -1,36 +1,191 @@
-// var domainName = document.domain;
-// var domain = domainName.replace('www.','');
+$( document ).ready(function() {
 
-// httpRequest = new XMLHttpRequest();
-// httpRequest.open('GET', '//fast-ocean-4567.herokuapp.com/api/notes?domain=' + domain, true);
-// // httpRequest.open('GET', 'http://localhost:3000/api/notes', true);
-// console.log("Hmmm...what's going on.");
+  $('#add-post-container').toggle();
 
-// httpRequest.onreadystatechange = function(){
-//   if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-//     console.log("AJAX request successfully made.");
-//     var myArr = JSON.parse(httpRequest.responseText);
-//     document.getElementById('postCC').innerHTML = stylize(myArr);
-//   }
-// };
+  $(document).on('click', '#hide-button', function() {
+    $('#wndx-sidebar').css("right", "-400px");
+    $('#show-button').css("right", "-20px");
+    $('.commentCC').css("right", "-380px");
+    $('.userCC').css("right", "-380px");
+    $('#sessionCC').css("right", "-380px");
+    $('#top-bar').css("right", "-380px");
+  });
 
-// httpRequest.send();
 
-// // TEMPLATE
-// // <div class="postContainer">
-// //   <p class="Post">
-// //     <span class="userName">@alexhart:</span> 
-// //     <span class="postBody">This is a four line post that I’m writing to check on this fonts legibility for body copy.</span>
-// //   </p>
-// //   <p class="commentsLink">44 Comments</p>
-// // </div>
 
-// function stylize(arr) {
-//   var out = "";
-//   for(var i = 0; i < arr.length; i++) {
-//     out += '<div class="postContainer"> <p class="Post"> <span class="userName">@' + 'USERNAME' + ':</span><span class="postBody">' + arr[i].description + '</span></p><p class="commentsLink">' + 'COMMENT COUNT' + '</p></div>';
-//   }
-//   return out;
-// }
 
-///////
+  // $(document).on('click', '#loginSubmitButton', function() {
+
+  //     // Ajax called for authentication
+  //    console.log('hello?');
+  //     var AUTH_TOKEN = $('meta[name="csrf-token"]').attr('content');
+  //     $.ajaxSetup({
+  //         beforeSend: function (xhr) {
+  //             xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+  //         }
+  //     });
+
+  //     $.ajax({
+  //         type: "POST",
+  //         url: "//fast-ocean-4567.herokuapp.com/login?&authenticity_token=" + encodeURIComponent(AUTH_TOKEN),
+  //         data: {
+  //             username: $('#username').val(),
+  //             password: $('#password').val()
+  //         },
+  //         dataType: "HTML",
+  //         success: function (data) {
+  //             console.log(data);
+  //             // window.location = data.redirect_url;
+  //         },
+  //         error: function (xhr, status) {
+  //             console.log(status);
+  //         }
+  //     });
+
+  //     return false;
+  // });
+
+
+
+ $(document).on('click', '#logout', function() {
+
+      // Ajax call for authentication
+     console.log('this is where we will put code to log out?');
+
+  });
+
+
+
+// Ajax call for creating a post
+$(document).on('click', '#submitPost', function () {
+
+    console.log('hello?');
+    var AUTH_TOKEN = $('meta[name="csrf-token"]').attr('content');
+    $.ajaxSetup({
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+        }
+    });
+
+    $.ajax({
+        type: "POST",
+        url: "//fast-ocean-4567.herokuapp.com/notes?&authenticity_token=" + encodeURIComponent(AUTH_TOKEN),
+        data: {
+            utf: "✓",
+            description: $('#add-post-input').val(),
+            domain: $('#domain').val(),
+            user_id: $('#user_id').val(),
+            commit: "submit"
+        },
+        dataType: "JSON",
+        success: function (data) {
+            console.log(data);
+            // window.location = data.redirect_url;
+        },
+        error: function (xhr, status) {
+            console.log(status);
+        }
+    });
+
+    return false;
+});
+
+
+
+
+
+  $('#show-button').click(function() {
+    $('#wndx-sidebar').css("right", "0px");
+    $('#show-button').css("right", "-100px");
+    $('#top-bar').css("right", "0px");
+  });
+
+  $(document).on('click', '#addPost', function() {
+    $('#postCC').css("margin-top", "280px");
+    $('#addPost').css("opacity", "0");
+    $('#addPost').toggle();
+    $('#add-post-container').toggle();
+    $('#add-post-input').css("opacity", "1");
+      $('#add-post-input').css("height", "80px");
+  });
+
+  $(document).on('click', '#submitPost', function() {
+    $('#postCC').css("margin-top", "90px");
+    $('#addPost').css("opacity", "1");
+    $('#add-post-container').toggle();
+    $('#add-post-input').css("opacity", "0");
+    $('#add-post-input').css("height", "0px");
+  });
+
+  $(document).on('click', '#closePost', function() {
+    $('#postCC').css("margin-top", "90px");
+    $('#addPost').css("opacity", "1");
+    $('#addPost').toggle();
+    $('#add-post-container').toggle();
+    $('#add-post-input').css("opacity", "0");
+    $('#add-post-input').css("height", "0px");
+  });
+
+  $(document).on('click', '.commentsLink', function() {
+
+    var keepItRel = $(this).attr('rel');
+    console.log($('.commentCC[rel='+keepItRel+']'));
+
+    $('.commentCC[rel='+keepItRel+']').css("right", "-10px");
+    $('.hide-comments-vert-button').css("right", "370px");
+  });
+
+  $(document).on('click', '#hide-comments-button', function() {
+    $('.commentCC').css("right", "-380px");
+    $('.hide-comments-vert-button').css("right", "0px");
+  });
+
+  $(document).on('click', '.hide-comments-vert-button', function() {
+    $('.commentCC').css("right", "-380px");
+    $('.hide-comments-vert-button').css("right", "0px");
+  });
+
+  $(document).on('mouseover', '.hide-comments-vert-button', function() {
+    $('.commentCC').css("border-left", "1px solid rgba(255,255,255,.8)");
+  });
+
+  $(document).on('mouseout', '.hide-comments-vert-button', function() {
+    $('.commentCC').css("border-left", "1px solid rgba(255,255,255,.1)");
+
+  });
+
+  $(document).on('click', '.userName', function() {
+
+    var keepItRel = $(this).attr('rel');
+    console.log($('.userCC[rel='+keepItRel+']'));
+
+    $('.userCC[rel='+keepItRel+']').css("right", "-10px");
+
+  });
+
+  $(document).on('click', '#hide-user-button', function() {
+    $('.userCC').css("right", "-380px");
+    $('.hide-user-vert-button').css("right", "0px");
+  });
+
+  $(document).on('click', '#login', function() {
+    $('#sessionCC').css("right", "-10px");
+      $('#login').toggle();
+    $('#signup').toggle();
+
+  });
+
+  $(document).on('click', '#signup', function() {
+    $('#sessionCC').css("right", "-10px");
+      $('#login').toggle();
+    $('#signup').toggle();
+  });
+
+  $(document).on('click', '#hide-session-button', function() {
+    $('#sessionCC').css("right", "-380px");
+    $('#login').toggle();
+    $('#signup').toggle();
+
+  });
+
+});
