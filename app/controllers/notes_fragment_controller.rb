b6e94d891ea0785cda4252838055bdeeaed7fb02
rajangdavis <rajangdavis@gmail.com>
2014-12-17 13:57:07 -0800

    updating repo

---
+++ app/controllers/notes_fragment_controller.rb
@@ -0,0 +1,28 @@
+class NotesFragmentController < FragmentController
+
+  helper_method :current_user
+
+  def show
+    @note = Note.get_multi(nil, params[:id]).first
+	end
+
+  def create
+    note = Note.new(note_params)
+
+    if note.save
+      @note = Note.get_multi(nil, note.id).first
+      render json: {info: render_to_string("layouts/_note.html.erb", locals: {note: @note}) }
+      # render json: note, status: :ok 
+    else
+    end
+  end
+
+  def note_params
+    params.permit(:description, :domain, :path, :user_id)
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+  end
+
+end
